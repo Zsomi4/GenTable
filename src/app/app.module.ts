81038c7153c4241c8fda16f79bf03261cbcf9d8f
Zsomi4 <k72302872172@ktch.hu>
2022-11-09 09:49:45 +0100

    Kezdés

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EmpComponent } from './emp/emp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpComponent
   ],
   imports: [
     BrowserModule
