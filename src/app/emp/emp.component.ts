8200a673674a73616a76614c78000326a7f4f420
Zsomi4 <k72302872172@ktch.hu>
2022-11-09 10:03:50 +0100

    employes tömb megírása.

--- src/app/emp/emp.component.ts
+++ src/app/emp/emp.component.ts
@@ -6,7 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./emp.component.css']
 })
 export class EmpComponent implements OnInit {
-
+  empoyes = [
+    {name: 'Ábor Elek', city: 'Szeged', salary: 384},
+    {name: 'Trank Gábor', city: 'Miskolc', salary: 322},
+    {name: 'Paprika József', city: 'Szolnok', salary: 349},
+    {name: 'Long Lajos', city: 'Budapest', salary: 335}
+  ]
   constructor() { }
 
   ngOnInit(): void {
